cfa7567a1bbc8584c8e63017c4a15dd1e52e27b2
Jay-Dhanwant <jaynaresh.dhanwant@gmail.com>
2024-07-04 15:19:25 +0530

    adding event_aggregator_dag.py

---
+++ dags/event_aggregator_dag.py
@@ -0,0 +1,61 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'retries': 1,
+}
+
+dag = DAG(
+    'temp_bigquery_aggregator',
+    default_args=default_args,
+    description='A temporary DAG to run BigQuery aggregation',
+    schedule_interval=None,
+)
+
+# Your SQL query as a string
+sql_query = """
+CREATE OR REPLACE TABLE `hot-or-not-feed-intelligence.analytics_views.daily_event_counts_table` AS
+WITH dated_events AS (
+  SELECT 
+    DATE(timestamp) AS event_date,
+    event as event_name,
+    *
+  FROM 
+    `hot-or-not-feed-intelligence.analytics_335143420.test_events_analytics`
+),
+
+last_10_days AS (
+  SELECT DISTINCT event_date
+  FROM dated_events
+  ORDER BY event_date DESC
+  LIMIT 100
+)
+
+SELECT 
+  d.event_date,
+  d.event_name,
+  COUNT(*) AS event_count
+FROM 
+  dated_events d
+JOIN 
+  last_10_days l ON d.event_date = l.event_date
+GROUP BY 
+  d.event_date,
+  d.event_name
+ORDER BY 
+  d.event_date DESC,
+  d.event_name
+"""
+
+run_query = BigQueryExecuteQueryOperator(
+    task_id='run_query',
+    sql=sql_query,
+    use_legacy_sql=False,
+    dag=dag,
+)
+
+run_query 
+
