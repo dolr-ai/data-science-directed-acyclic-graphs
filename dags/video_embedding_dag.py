137485280e18ca4e00a56f20df5a8f799305f0c3
komal-rs <komalsai@gobazzinga.io>
2024-07-04 20:14:33 +0530

    video embed 3

--- dags/video_embedding_dag.py
+++ dags/video_embedding_dag.py
@@ -17,14 +17,55 @@ default_args = {
 
 # Your SQL query as a string
 create_embed_query = """
-CREATE OR REPLACE TABLE `hot-or-not-feed-intelligence.test_yral_video.video_embeddings` AS 
+MERGE `hot-or-not-feed-intelligence.test_yral_video.video_embeddings` AS target
+USING
 (SELECT *
 FROM ML.GENERATE_EMBEDDING(
   MODEL `hot-or-not-feed-intelligence.test_yral_video.mm_embed`,
   TABLE `hot-or-not-feed-intelligence.test_yral_video.video_object_table`,
   STRUCT(TRUE AS flatten_json_output,
     10 AS interval_seconds)
-));
+)) AS source
+on target.uri = source.uri
+WHEN MATCHED THEN
+  UPDATE SET
+    ml_generate_embedding_result = source.ml_generate_embedding_result,
+    ml_generate_embedding_status = source.ml_generate_embedding_status,
+    ml_generate_embedding_start_sec = source.ml_generate_embedding_start_sec,
+    ml_generate_embedding_end_sec = source.ml_generate_embedding_end_sec,
+    generation = source.generation,
+    content_type = source.content_type,
+    size = source.size,
+    md5_hash = source.md5_hash,
+    updated = source.updated,
+    metadata = source.metadata
+WHEN NOT MATCHED THEN
+  INSERT (
+    ml_generate_embedding_result,
+    ml_generate_embedding_status,
+    ml_generate_embedding_start_sec,
+    ml_generate_embedding_end_sec,
+    uri,
+    generation,
+    content_type,
+    size,
+    md5_hash,
+    updated,
+    metadata
+  )
+  VALUES (
+    source.ml_generate_embedding_result,
+    source.ml_generate_embedding_status,
+    source.ml_generate_embedding_start_sec,
+    source.ml_generate_embedding_end_sec,
+    source.uri,
+    source.generation,
+    source.content_type,
+    source.size,
+    source.md5_hash,
+    source.updated,
+    source.metadata
+  );
 """
 
 
@@ -47,26 +88,41 @@ with DAG(
         dag=dag,
     )
 
-    get_data = BigQueryGetDataOperator(
-        task_id="get_data_from_bq",
+    get_data_object_table = BigQueryGetDataOperator(
+        task_id="get_data_from_bq_object_table",
+        dataset_id="test_yral_video",
+        table_id="video_object_table",
+        table_project_id="hot-or-not-feed-intelligence",
+        max_results=500000,
+        selected_fields="uri",
+    )
+
+    get_data_embeddings = BigQueryGetDataOperator(
+        task_id="get_data_from_bq_embeddings",
         dataset_id="test_yral_video",
         table_id="video_embeddings",
         table_project_id="hot-or-not-feed-intelligence",
-        max_results=500000,
+        max_results=1000000,
         selected_fields="uri",
     )
 
     # Step 2: Process the list to extract object names
     def extract_object_names(**kwargs):
         task_instance = kwargs["task_instance"]
-        rows = task_instance.xcom_pull(task_ids="get_data_from_bq")
+        obj_table_rows = task_instance.xcom_pull(
+            task_ids="get_data_from_bq_object_table"
+        )
+        embedding_rows = task_instance.xcom_pull(task_ids="get_data_from_bq_embeddings")
+
+        obj_table_names = [row[0] for row in obj_table_rows]
+        embedding_names = set([row[0] for row in embedding_rows])
 
         object_names = []
-        for row in rows:
-            uri = row[0]
-            # Extract object name from URI
-            object_name = uri.split("gs://")[1].split("/", 1)[1]
-            object_names.append(object_name)
+        for uri in obj_table_names:
+            if uri in embedding_names:
+                # Extract object name from URI
+                object_name = uri.split("gs://")[1].split("/", 1)[1]
+                object_names.append(object_name)
 
         # Push the list of object names to XCom
         task_instance.xcom_push(key="object_names", value=object_names)
