e2bcbd5a6d083a0db159e418a152bee8c8fdd5b4
jay-dhanwant <jay@gobazzinga.io>
2024-08-26 01:42:45 +0530

    changing dags for HON game

--- user_video_metrics/ds__user_video_metrics.py
+++ user_video_metrics/ds__user_video_metrics.py
@@ -121,7 +121,7 @@ default_args = {
     'retries': 1,
 }
 
-with DAG('user_video_metrics_dag', default_args=default_args, schedule_interval='*/15 * * * *', catchup=False) as dag:
+with DAG('user_video_metrics', default_args=default_args, schedule_interval='*/15 * * * *', catchup=False) as dag:
     run_query_task = PythonOperator(
         task_id='run_query_task',
         python_callable=run_query,
