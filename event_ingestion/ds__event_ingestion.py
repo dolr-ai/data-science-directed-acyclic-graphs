899fc9713e05d6303b2266cc5c564a06720d2b18
jay-dhanwant <jay.hdfs@gmail.com>
2025-07-11 01:34:25 +0530

    checkpoint: changing cluster config

--- event_ingestion/ds__event_ingestion.py
+++ event_ingestion/ds__event_ingestion.py
@@ -101,7 +101,7 @@ CLUSTER_CONFIG = {
         "properties": {
             "spark:spark.dataproc.enhanced.optimizer.enabled": "true",
             "spark:spark.dataproc.enhanced.execution.enabled": "true",
-            "spark.datasource.bigquery.temporaryGcsBucket": GCS_BUCKET   # <── NEW
+            "spark:spark.datasource.bigquery.temporaryGcsBucket": GCS_BUCKET   
 
         },
     },
